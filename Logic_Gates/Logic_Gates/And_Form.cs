6ba131237a25af0a4dd0d3be7c183dedff9b9f64
Sayed_Abd_Elaleem_Sayed <30577812+SayedAbdElaleem@users.noreply.github.com>
2017-08-24 12:39:22 -0700

    Logic_Gates
    This program Showing how Logic Gates Doing

---
+++ Logic_Gates/Logic_Gates/And_Form.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logic_Gates
+{
+    public partial class And_Form : Form
+    {
+        public And_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox_OR_A_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_OR_A.Checked == false || checkBox_OR_B.Checked == false)
+            {
+                pictureBox_OR.Visible = false;
+                pictureBox_OR1.Visible = true;
+            }
+            else if (checkBox_OR_A.Checked == true && checkBox_OR_B.Checked == true)
+            {
+                pictureBox_OR.Visible = true;
+                pictureBox_OR1.Visible = false;
+            }
+        }
+
+        private void checkBox_OR_B_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_OR_B.Checked == false || checkBox_OR_A.Checked == false)
+            {
+                pictureBox_OR.Visible = false;
+                pictureBox_OR1.Visible = true;
+            }
+            else if (checkBox_OR_B.Checked == true && checkBox_OR_A.Checked == true)
+            {
+                pictureBox_OR.Visible = true;
+                pictureBox_OR1.Visible = false;
+            }
+        }
+        private void Btn_Home_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 back = new Form1();
+            back.ShowDialog();
+            this.Close();
+        }
+
+        private void Btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
---
+++ Logic_Gates/Logic_Gates/OR_Form.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logic_Gates
+{
+    public partial class OR_Form : Form
+    {
+        public OR_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void OR_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_OR_A.Checked == true || checkBox_OR_B.Checked == true)
+            {
+                pictureBox_OR.Visible = true;
+                pictureBox_OR1.Visible = false;
+            }
+            else if (checkBox_OR_A.Checked == false && checkBox_OR_B.Checked == false)
+            {                
+                pictureBox_OR.Visible = false;
+                pictureBox_OR1.Visible = true;
+            }
+        }
+
+        private void checkBox_OR_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_OR_B.Checked == true || checkBox_OR_A.Checked == true)
+            {
+                pictureBox_OR.Visible = true;
+                pictureBox_OR1.Visible = false;
+            }
+            else if (checkBox_OR_B.Checked == false && checkBox_OR_A.Checked == false)
+            {                
+                pictureBox_OR.Visible = false;
+                pictureBox_OR1.Visible = true;
+            }
+
+        }
+
+        private void Btn_Home_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 back = new Form1();
+            back.ShowDialog();
+            this.Close();
+        }
+
+        private void Btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
